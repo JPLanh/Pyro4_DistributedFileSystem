1ae5c678e9831b460e5d3dd807edeb283f017471
JPLanh <JPLanh@yahoo.com>
2018-05-24 23:58:48 -0700

    Hybrid Chord
    Made the chord into a hybrid, so it can be a server and a client

--- Chord.py
+++ Chord.py
@@ -1,6 +1,9 @@
 import Pyro4
+from Pyro4 import naming
 import hashlib
 import os
+import threading
+import time
 
 @Pyro4.expose
 class Chord(object):
@@ -23,7 +26,20 @@ class Chord(object):
     def remove(self, item):
         self.list.remove(item)
 
+class start_server(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        print("Thread started")
+        Pyro4.naming.startNSloop()
+            
 if __name__ == "__main__":
+    try:
+        thread1 = start_server()
+        thread1.start()
+    except:
+        print("Unable to start thread")
     getIP = input("IP:").strip()
     getPort = int(input("Port:").strip())
     chord = Chord(getIP, getPort)
