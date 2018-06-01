9de1e8c16c6496d9cb170508031cb2db5a7ecdc0
JPLanh <JPLanh@yahoo.com>
2018-05-31 17:32:45 -0700

    implement some chord core feature
    Impelmented join, notify, stabilize, fixfinger, and their respective helper functions.
    One chord is able to join another chord and is able to "traverse" each chord

--- Chord.py
+++ Chord.py
@@ -9,9 +9,18 @@ import time
 @Pyro4.expose
 class Chord(object):
     def __init__(self, ip, port, guid):
+        self.M = 3
         self._ip = ip
         self._port = port
         self._guid = guid
+        self.successor = self
+        self._predecessor = None
+        self.finger = []
+        self.nextFinger = 0
+        for i in range(0, self.M):
+            self.finger.append(None)
+        thread1 = looping(self)
+        thread1.start()
 
     @property
     def ip(self):
@@ -25,6 +34,84 @@ class Chord(object):
     def guid(self):
         return self._guid
 
+    @property
+    def chord(self):
+        return self._chord
+
+    @property
+    def predecessor(self):
+        return self._predecessor
+
+    def stabilize(self):
+        if self.successor != None:
+            x = self.successor.predecessor
+            if x != None and x.guid != self._guid and self.inCloseInterval(x.guid, self._guid, self.successor.guid):
+                self.successor = x
+           # if self.successor.guid != self._guid:
+            self.successor.notify(self)
+
+    def notify(self, chord):
+        if self._predecessor == None or (self._predecessor != None and self.inCloseInterval(chord.guid, self._predecessor.guid, self._guid)):
+            self._predecessor = chord
+
+    def fixFinger(self):
+        nextGuid = self._guid + 1 << (self.nextFinger+1)
+        self.finger[self.nextFinger] = self.locateSuccessor(nextGuid)
+        self.nextFinger = (self.nextFinger + 1)%self.M
+    
+    def isAlive(self):
+        return True
+    
+    def checkPredecessor(self):
+        if self._predecessor != None and not self._predecessor.isAlive():
+            self._predecessor = None
+
+    def inCloseInterval(self, guid, begin, end):
+        if begin < end:
+            return guid > begin and guid < end
+        else:
+            return guid > begin or guid < end
+
+    def locateSuccessor(self, guid):
+        if guid == self._guid:
+            print("Error it's the same shit")
+        else:
+            if self.successor.guid != guid:
+                if self.inCloseInterval(guid, self._guid, self.successor.guid):
+                    return self.successor
+                nextSuccessor = self.closestPrecedingChord(guid)
+                if nextSuccessor == None:
+                    return None
+                return nextSuccessor.locateSuccessor(guid)
+
+    def printFinger(self):
+        for i in self.finger:
+            print(i)
+
+    def closestPrecedingChord(self, guid):
+        if guid != self._guid:
+            i = self.M - 1;
+            while i >= 0:
+                if self.inCloseInterval(self.finger[i].guid, self._guid, guid):
+                    if self.finger[i].guid != guid:
+                        return self.finger[i]
+            return self.successor
+
+    def simplePrint(self):
+        if self.predecessor != None:
+            print("S: %s C: %s P: %s" %(self.successor.guid, self.guid, self._predecessor.guid))
+        else:
+            print("S: %s C: %s P: %s" %(self.successor.guid, self.guid, self._predecessor))
+            
+
+    def joinRing(self, guid):
+        with Pyro4.locateNS() as ns:
+            for guidGet, guidURI in ns.list(prefix=str(guid)).items():
+                chordGet = Pyro4.Proxy(guidURI)
+                self._predecessor = None
+                self.successor = chordGet.locateSuccessor(self._guid)
+                print("Joining Ring")                 
+
     def readMetaData(self):
         jread = open(self._guid + "/repository/metadata", 'r')
         jsonRead = json.load(jread)
@@ -37,12 +124,13 @@ class Chord(object):
         json.dump(metadata, f)
         f.close()
 
-    def joinRing(self, guid):
-        with Pyro4.locateNS() as ns:
-            for guidGet, guidURI in ns.list(prefix=guid).items():
-                print(Pyro4.Proxy(guidURI))
-                
-                
+    def ringAround(self, initial, count):
+        if self.guid != initial.guid:
+            print("%i : %s" %(count, initial.guid))
+            self.successor.ringAround(initial, count+1)
+        else:
+            print("%i : %s" %(count, initial.guid))            
+        
     def newFile(self, file):
         metadata = self.readMetaData()
         fileInfo = {}
@@ -60,3 +148,15 @@ class Chord(object):
 
     def remove(self, item):
         self.list.remove(item)
+
+class looping(threading.Thread):
+    def __init__(self, chord):
+        threading.Thread.__init__(self)
+        self.chord = chord
+
+    def run(self):
+        while True:
+            self.chord.stabilize()
+            self.chord.fixFinger()
+            self.chord.checkPredecessor()
+            time.sleep(5)
--- Client.py
+++ Client.py
@@ -7,13 +7,34 @@ import time
 import json
 from Chord import Chord
 
-class start_server(threading.Thread):
+class start_name_server(threading.Thread):
     def __init__(self):
-        threading.Thread.__init__(self)
+      threading.Thread.__init__(self)
 
     def run(self):
-        print("Thread started")
-        Pyro4.naming.startNSloop()
+      Pyro4.naming.startNSloop()
+      
+class start_server(threading.Thread):
+    def __init__(self, ip, port, chord):
+        threading.Thread.__init__(self)
+        self._ip = ip
+        self._port = port
+        self._chord = chord
+
+    def run(self):
+      with Pyro4.Daemon(host=self._ip, port = self._port) as daemon:
+          chordURI = daemon.register(self._chord)
+          directory = os.path.dirname(str(self._chord.guid)+"/repository/")
+          if not os.path.exists(directory):
+            os.makedirs(directory)
+          try:
+            f = open(directory+"/metadata", 'r')
+          except:
+            createMeta(directory)
+          with Pyro4.locateNS() as ns:
+            ns.register(str(self._chord.guid), chordURI)
+          print("Thread started")
+          daemon.requestLoop()
 
 def createMeta(path):
   print("File was not found")
@@ -40,7 +61,10 @@ def prompt(chord):
             print('Upload \t\t up \t  {filename} \t Upload the specifed filename')
             print('Download \t down \t  {filename} \t Download the specifed filename')
             print('Exit \t\t exit \t \t\t Exit from the system')
-#        elif choiceSplit[0].lower() == "ls":
+        elif choiceSplit[0].lower() == "ls":
+          chord.simplePrint()
+        elif choiceSplit[0].lower() == "ring":
+          chord.successor.ringAround(chord, 0)
     elif len(choiceSplit) > 1:
         if choiceSplit[0].lower() == "up":
             fileName = getChoice[3:]
@@ -49,37 +73,25 @@ def prompt(chord):
             m = hashlib.md5()
             IPGet = choiceSplit[1] + ":" + str(choiceSplit[2])
             m.update(IPGet.encode('utf-8'))  
-            chord.joinRing(m.hexdigest())
+            chord.joinRing(int(m.hexdigest(), 16))
     
 if __name__ == "__main__":
+#    nameServer = start_name_server()
+#    nameServer.start()
     getIP = input("IP:")
     getPort = int(input("Port:"))
 #    getIP = 'localhost'
 #    getPort = 23245
     #try:
-    thread1 = start_server()
-    thread1.start()
-    with Pyro4.Daemon(host=getIP, port = getPort) as daemon:
-        with Pyro4.locateNS() as ns:
-            m = hashlib.md5()
-            IPGet = getIP + ":" + str(getPort)
-            m.update(IPGet.encode('utf-8'))  
-            chord = Chord(getIP, getPort, m.hexdigest())
-            chordURI = daemon.register(chord)          
-            directory = os.path.dirname(str(m.hexdigest())+"/repository/")
-            if not os.path.exists(directory):
-              os.makedirs(directory)
-            try:
-              f = open(directory+"/metadata", 'r')
-            except:
-              createMeta(directory)
-            ns.register(str(m.hexdigest()), chordURI)
-            print("Welcome User!")
+    m = hashlib.md5()
+    IPGet = getIP + ":" + str(getPort)
+    m.update(IPGet.encode('utf-8'))
+    guid = int(m.hexdigest(), 16)
+    chord = Chord(getIP, getPort, guid)
+    node = start_server(getIP, getPort, chord)
+    node.start()
+    time.sleep(10)
+    print("Welcome User!")
     while True:
-        prompt(chord)
+         prompt(chord)
 
-    #except:
-    #    print("Unable to start thread")
-
-##    with Pyro4.locateNS() as ns:
-##        print(ns.list())
