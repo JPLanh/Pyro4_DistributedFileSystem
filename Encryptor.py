75b0fc49d41737026818740bbb45593ad39437b6
JPLanh <JPLanh@yahoo.com>
2018-06-23 23:19:28 -0700

    Update Encryptor.py

--- Encryptor.py
+++ Encryptor.py
@@ -1,43 +1,60 @@
 import os
-from base64 import b64decode
+from base64 import b64encode, b64decode
 import constant
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.padding import PKCS7
 from cryptography.hazmat.primitives import serialization, hashes, hmac, asymmetric, padding
 from cryptography.exceptions import InvalidSignature
-from datetime import datetime
+import Logger
 
 def dataEncrypt(message, encKey, hMacKey):
+    Logger.log("Flag 4.5.1")
     if len(encKey) == constant.KEY_BYTE_SIZE:
         if len(hMacKey) == constant.KEY_BYTE_SIZE:
-            logger("Flag 5.1.1")
             IV = os.urandom(constant.IV_BYTE_SIZE)
             cipher = Cipher(algorithms.AES(encKey), modes.CBC(IV), backend=default_backend())
             cipherEncrypt = cipher.encryptor()
-            logger("Flag 5.1.2")
             pad = padding.PKCS7(constant.PADDING_BLOCK_SIZE).padder()
             cipherText = pad.update(message) + pad.finalize()
-            logger("Flag 5.1.3")
             cipherText = cipherEncrypt.update(cipherText) + cipherEncrypt.finalize()
             hTag = hmac.HMAC(hMacKey, hashes.SHA256(), backend=default_backend())
-            logger("Flag 5.1.4")
             hTag.update(cipherText)
             hTag = hTag.finalize()
             return cipherText, IV, hTag
 
 def chainEncryption(message, tag, encKey, hMacKey):
+    Logger.log("Flag 4.1")
     checkTag = hmac.HMAC(hMacKey, hashes.SHA256(), backend=default_backend())
+    Logger.log("Flag 4.2")
     checkTag.update(message)
+    Logger.log("Flag 4.3")
     try:
+        Logger.log("Flag 4.4")
         checkTag.verify(tag)
+        Logger.log("Flag 4.5")
         newEncKey = os.urandom(constant.KEY_BYTE_SIZE)
         newHMacKey = os.urandom(constant.KEY_BYTE_SIZE)
-        return dataEncrypt(message, newEncKey, newHMacKey), newEncKey, hMacKey
+        cipherText, newIV, newHTag = dataEncrypt(message, newEncKey, newHMacKey)
+        return cipherText, newIV, newHTag, newEncKey, newHMacKey
     except InvalidSignature:
+        Logger.log("Failed")
         return None
 
-def chainInitialize(RSACipher, cipherText, IV, tag, count):
+def chainInitialize(message):
+    encryptionSet = []
+    Logger.log("Flag 1")
+    RSACipher, cipherText, IV, tag = initialize(message)
+    newSet = {}
+    newSet["Set"] = 0
+    newSet["RSACipher"] = b64encode(RSACipher).decode('utf-8')
+    newSet["IV"] = b64encode(IV).decode('utf-8')
+    newSet["Tag"] = b64encode(tag).decode('utf-8')
+    encryptionSet.append(newSet)
+    Logger.log("Flag 2")    
+    return encryptChaining(cipherText, encryptionSet, 1)
+    
+def encryptChaining(cipherText, RSASet, count):
     f=open(constant.PRIVATE_PEM, 'rb')
     private_key = serialization.load_pem_private_key(
         f.read(),
@@ -45,6 +62,13 @@ def chainInitialize(RSACipher, cipherText, IV, tag, count):
         backend=default_backend()
     )
 
+    f.close()
+ 
+    for x in RSASet:
+        if x["Set"] == count - 1:
+             RSACipher = b64decode(x["RSACipher"])
+             tag = b64decode(x["Tag"])
+             
     key = private_key.decrypt(
         RSACipher,
         asymmetric.padding.OAEP(
@@ -53,48 +77,55 @@ def chainInitialize(RSACipher, cipherText, IV, tag, count):
             label=None
         )
     )
+    
+    encKey = key[:32]
+    hMacKey = key[32:]
+    
+    newCipher, newIV, newTag, newEncKey, newHMacKey = chainEncryption(cipherText, tag, encKey, hMacKey)
 
-    encKey = key[:len(key)/2]
-    hMacKey = key[len(key)/2:]
+    f=open(constant.PUBLIC_PEM, 'rb')
+    public_key = serialization.load_pem_public_key(
+        f.read(),
+        backend=default_backend()
+    )
 
-    newCipher, newIV, newTag, newEnc, newHMac = chainEncryption(cipherText, tag, encKey[:32], hMacKey[:32])
+    f.close()
 
-    combEncKey = newEnc + encKey
-    combHMacKey = newHMac + hMacKey
-    combIV = newIV + IV
-
-    if newCipher != None:
-        f=open(constant.PUBLIC_PEM, 'rb')
-        public_key = serialization.load_pem_public_key(
-            f.read(),
-            backend=default_backend()
-        )
-
-        RSACipher = public_key.encrypt(
-            combEncKey+combHMacKey,
-            asymmetric.padding.OAEP(
-                mgf=asymmetric.padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-                )
+    RSACipher = public_key.encrypt(
+        newEncKey+newHMacKey,
+        asymmetric.padding.OAEP(
+            mgf=asymmetric.padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
             )
-        
-    return RSACipher, newCipher, combIV, newTag
+        )
+    Logger.log("Flag 5")
+    newSet = {}
+    newSet["Set"] = count
+    newSet["RSACipher"] = b64encode(RSACipher).decode('utf-8')
+    newSet["IV"] = b64encode(newIV).decode('utf-8')
+    newSet["Tag"] = b64encode(newTag).decode('utf-8')
+    RSASet.append(newSet)
+    Logger.log("Flag 6")
+    
+    if count == constant.MAX_CHAIN_ENCRYPTION: 
+        Logger.log("Flag 8")       
+        return newCipher, RSASet
+    else:
+        Logger.log("Flag 7")
+        return encryptChaining(newCipher, RSASet, count + 1)
    
 
 def initialize(message):
     encKey = os.urandom(constant.KEY_BYTE_SIZE)
     hMacKey = os.urandom(constant.KEY_BYTE_SIZE)
-    logger("Flag 5.1")
     cipherText, IV, tag = dataEncrypt(message, encKey, hMacKey)
-    logger("Flag 5.2")
     if cipherText != None:
         f=open(constant.PUBLIC_PEM, 'rb')
         public_key = serialization.load_pem_public_key(
             f.read(),
             backend=default_backend()
         )
-        logger("Flag 5.3")
 
         RSACipher = public_key.encrypt(
             encKey+hMacKey,
@@ -104,14 +135,4 @@ def initialize(message):
                 label=None
                 )
             )
-        logger("Flag 5.4")
         return RSACipher, cipherText, IV, tag
-
-
-def logger(data):
-    try:
-        f = open("Logger.txt", 'a+')
-    except:
-        f = open("Logger.txt", 'w+')
-    f.write("[" + str(datetime.now()) + "] " + data + "\n")
-    f.close()
